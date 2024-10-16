7550011b691d42d7c7add800eb840c769bd82780
shumoncmt <shomoncmt@gmail.com>
2024-10-16 18:15:02 +0600

    add

--- student_result/result.php
+++ student_result/result.php
@@ -1,10 +1,30 @@
 <?php
-$bangla = 80;
-$english = 66;
-$math = 87;
-$socialSeance = 63;
-$chemistry = 45;
-switch(){
+$bangla = 100;
+$english = 100;
+$math = 100;
+$socialSeance = 100;
+$chemistry = 100;
 
+
+$argv = ($bangla+$chemistry+$english+$math+$socialSeance)/5;
+
+$grade = ' ';
+
+switch($argv){
+    case($argv >= 80):
+        echo "A+";
+        break;
+        case($argv >= 70):
+            echo "A";
+            break;
+         case($argv >= 60):
+             echo "B";
+              break;
+         case($argv >= 50):
+             echo "B";
+                 break;
 
 }
+
+echo $argv ;
+echo $grade;
