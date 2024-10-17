3a80f03a13122cc874dfbe0e3fd7df8e23d84675
Md. Shumon Mia <shomoncmt@gmail.com>
2024-10-17 22:31:30 +0600

    add
    Add project

--- student_result/result.php
+++ student_result/result.php
@@ -1,13 +1,13 @@
 <?php
-$bangla = 33;
+$bangla = 80;
 $english = 80;
-$math = 0;
-$socialSeance = 58;
-$chemistry = 33;
+$math = 80;
+$socialSeance = 80;
+$chemistry = 0;
 
 // $allSubject array= ($bangla, $english, $math, $socialSeance, $chemistry);
 
-
+$totalMarks = ($bangla+$english+$math+$socialSeance+$chemistry);
 $argv = ($bangla+$chemistry+$english+$math+$socialSeance)/5;
 
 
@@ -18,23 +18,49 @@ switch(true){
     case (($bangla >= 33 && $bangla <= 100 ) && ($english >= 33 && $english <= 100) && ($math >= 33 && $math <= 100) && ($socialSeance >= 33 && $socialSeance <= 100) && ($chemistry >= 33 && $chemistry <= 100)):  
         switch($argv){
             case($argv >= 80 && $argv <= 100): 
+                echo"Total marks $totalMarks";
+                echo "<br>";
+                echo "Average  marks $argv";
+                echo "<br>";
                 echo "Average Grade A+";
                 break;
                 case($argv >= 70 && $argv <= 79):
+                    echo"Total marks $totalMarks";
+                    echo "<br>";
+                    echo "Average  marks $argv";
+                    echo "<br>";
                     echo "Average Grade A";
                     break;
                  case($argv >= 60 && $argv <= 69):
+                    echo"Total marks $totalMarks";
+                    echo "<br>";
+                    echo "Average  marks $argv";
+                    echo "<br>";
                      echo "Average Grade B";
                       break;
                 case($argv >= 50 && $argv <= 59):
+                    echo"Total marks $totalMarks";
+                    echo "<br>";
+                    echo "Average  marks $argv";
+                    echo "<br>";
                     echo " Average Grade C";
                     break; 
                 case($argv >= 40 && $argv <= 49):
+                    echo"Total marks $totalMarks";
+                    echo "<br>";
+                    echo "Average  marks $argv";
+                    echo "<br>";
                    echo " Average Grade D";
                   break; 
                 case($argv >= 33 && $argv <= 39):
-                    echo " Average Grade E";
+                    echo"Total marks $totalMarks";
+                    echo "<br>";
+                    echo "Average  marks $argv";
+                    echo "<br>";
+                    echo "Average Grade E";
+                    
                     break;
+            
                 default:
                 echo "Average Grade fail";
         
@@ -43,6 +69,8 @@ switch(true){
         break;
 
         default:
+        
+        echo "\n";
         echo "You are fails";   
 
 };
