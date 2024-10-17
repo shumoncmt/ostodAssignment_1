f2f12fe553afd913507a1d059285f117f465ddc4
shumoncmt <shomoncmt@gmail.com>
2024-10-17 15:24:08 +0600

    add

--- student_result/result.php
+++ student_result/result.php
@@ -1,30 +1,47 @@
 <?php
-$bangla = 100;
-$english = 100;
-$math = 100;
-$socialSeance = 100;
-$chemistry = 100;
+$bangla = 33;
+$english = 33;
+$math = 33;
+$socialSeance = 33;
+$chemistry = 33;
 
 
 $argv = ($bangla+$chemistry+$english+$math+$socialSeance)/5;
 
-$grade = ' ';
+
+echo "All Subject Average Mark $argv" ;
+echo "\n";
+
+
 
 switch($argv){
-    case($argv >= 80):
-        echo "A+";
+    case($argv >= 80 && $argv <= 100):
+        echo "Average Grade A+";
         break;
-        case($argv >= 70):
-            echo "A";
+        case($argv >= 70 && $argv <= 79):
+            echo "Average Grade A";
             break;
-         case($argv >= 60):
-             echo "B";
+         case($argv >= 60 && $argv <= 69):
+             echo "Average Grade B";
               break;
-         case($argv >= 50):
-             echo "B";
-                 break;
+        case($argv >= 50 && $argv <= 59):
+            echo " Average Grade C";
+            break; 
+        case($argv >= 40 && $argv <= 49):
+           echo " Average Grade D";
+          break; 
+        case($argv >= 33 && $argv <= 39):
+            echo " Average Grade E";
+            break; 
+        default:
+        echo "Average Grade fail";
 
 }
+echo "\n";
 
-echo $argv ;
-echo $grade;
+
+
+
+ 
+
+
