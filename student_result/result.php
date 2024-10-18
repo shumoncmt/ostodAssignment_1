0476216ea95e4174bce7d6b37629a87d4d848bf6
Md. Shumon Mia <shomoncmt@gmail.com>
2024-10-18 20:44:09 +0600

    last Update
    Update

--- student_result/result.php
+++ student_result/result.php
@@ -73,37 +73,4 @@ switch(true){
         echo "\n";
         echo "You are fails";   
 
-};
-
-
-// switch($argv){
-//     case($argv >= 80 && $argv <= 100): 
-//         echo "Average Grade A+";
-//         break;
-//         case($argv >= 70 && $argv <= 79):
-//             echo "Average Grade A";
-//             break;
-//          case($argv >= 60 && $argv <= 69):
-//              echo "Average Grade B";
-//               break;
-//         case($argv >= 50 && $argv <= 59):
-//             echo " Average Grade C";
-//             break; 
-//         case($argv >= 40 && $argv <= 49):
-//            echo " Average Grade D";
-//           break; 
-//         case($argv >= 33 && $argv <= 39):
-//             echo " Average Grade E";
-//             break; 
-//         default:
-//         echo "Average Grade fail";
-
-// }
-echo "\n";
-
-
-
-
- 
-
-
+}
